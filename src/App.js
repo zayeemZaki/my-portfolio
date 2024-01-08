16b69120ad9d58f3acc161e35be52cbd2e31d38b
zayeemZaki <zayeemzaki45@gmail.com>
2024-01-07 23:02:05 -0500

    animation

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   useEffect(() => {
     AOS.init({
       duration: 2000,
-      once: true, // Whether animation should happen only once - while scrolling down
       mirror: false, // Whether elements should animate out while scrolling past them
     });
   }, []);
